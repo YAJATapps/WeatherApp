019dc3e204853d317ab7e730ec09474d72f0a8ac
YAJATapps <yajatapps3@gmail.com>
2020-11-10 02:11:13 +0530

    Show alerts if they're available from OpenWeatherMap.

--- app/src/main/java/com/groupk/weatherapp/ui/alerts/AlertsFragment.java
+++ app/src/main/java/com/groupk/weatherapp/ui/alerts/AlertsFragment.java
@@ -7,29 +7,58 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.groupk.weatherapp.R;
+import com.groupk.weatherapp.util.APIKey;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.HashMap;
+import java.util.Map;
+
+// Created by Yajat
 public class AlertsFragment extends Fragment {
-
-    private AlertsViewModel alertsViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        alertsViewModel =
-                new ViewModelProvider(this).get(AlertsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_alerts, container, false);
         final TextView textView = root.findViewById(R.id.text_alerts);
-        alertsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+        textView.setText(R.string.no_alerts);
+
+        new Thread(() -> {
+            String urlString = "https://api.openweathermap.org/data/2.5/weather?q=Kamloops" + "&appid=" + APIKey.getKEY() + "&units =metric";
+            try {
+                StringBuilder result = new StringBuilder();
+                URL url = new URL(urlString);
+                URLConnection conn = url.openConnection();
+                BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String line;
+                while ((line = rd.readLine()) != null) {
+                    result.append(line);
+                }
+
+                rd.close();
+
+                Map<String, Object> respMap = jsonToMap(result.toString());
+                Map<String, Object> alertsMap = jsonToMap(respMap.get("alerts").toString());
+
+                textView.setText(alertsMap.get("description").toString());
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        });
+        }).start();
+
         return root;
+    }
+
+    private Map<String, Object> jsonToMap(String str) {
+        return new Gson().fromJson(str, new
+                TypeToken<HashMap<String, Object>>() {
+                }.getType());
     }
 }
